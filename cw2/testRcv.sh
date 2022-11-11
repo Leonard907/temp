3b99d54d1d39b4dd3bf0fd773f5432041f9e2967
Leonard907 <ou_litu@163.com>
2022-11-11 12:33:45 +0000

    add

---
+++ cw2/testRcv.sh
@@ -0,0 +1,99 @@
+python3 Receiver4.py 54363 testReceive.jpg 1
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 1
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 1
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 1
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 1
+sleep 1
+echo "====================="
+python3 Receiver4.py 54363 testReceive.jpg 2
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 2
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 2
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 2
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 2
+sleep 1
+echo "====================="
+python3 Receiver4.py 54363 testReceive.jpg 4
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 4
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 4
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 4
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 4
+sleep 1
+echo "====================="
+python3 Receiver4.py 54363 testReceive.jpg 8
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 8
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 8
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 8
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg 8
+sleep 1
+echo "====================="
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+echo "====================="
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+echo "====================="
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+echo "====================="
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+echo "====================="
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+python3 Receiver4.py 54363 testReceive.jpg
+sleep 1
+echo "====================="
